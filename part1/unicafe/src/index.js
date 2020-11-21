5ff0ca07f32b34b44ad586313e83e1e852fa7e58
moryama171 <55793984+moryama171@users.noreply.github.com>
2020-11-21 10:37:55 +0100

    Add buttons

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Title = () => <h1>give feedback</h1>
+const Title = () => <h1>give feedback</h1>;
+
+const Button = ({ handleEvent, text }) => {
+  return (
+    <div>
+      <button onClick={handleEvent}>{text}</button>
+    </div>
+  )
+}
 
 const App = () => {
   
@@ -9,9 +17,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const setToGood = () => setGood(good + 1);
+  const setToNeutral = () => setNeutral(neutral + 1);
+  const setToBad = () => setBad(bad + 1);
+
   return (
     <div>
       <Title />
+      <Button handleEvent={setToGood} text='good'/>
+      <Button handleEvent={setToNeutral} text='neutral'/>
+      <Button handleEvent={setToBad} text='bad'/>
     </div>
   )
 }
