cf6b7d27f8f57703480f8c998c3388571d83eb99
moryama171 <55793984+moryama171@users.noreply.github.com>
2020-11-23 17:29:30 +0100

    Use .reduce() to calculate sum of exercises

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -28,8 +28,8 @@ const Content = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  let total = 0
-  course.parts.map(part => total += part.exercises)
+  const exercises = course.parts.map(part => part.exercises);
+  const total = exercises.reduce((a, b) => a + b);
   return (
     <>
       <p><b>total of {total} exercises</b></p>
