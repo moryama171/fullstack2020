85c3f8dc803a86c02abaaec2e307ef0c040391ab
moryama171 <55793984+moryama171@users.noreply.github.com>
2020-11-25 17:07:40 +0100

    Allow to add phone number

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,40 +2,54 @@ import React, { useState } from 'react';
 
 const App = () => {
     const [persons, setPersons ] = useState([
-        {name: 'Arto Hellas'}
+        {name: 'Arto Hellas',
+         number: '39-00-1234567'}
     ]);
+
     const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
 
-    const addNewName = (event) => {
+
+    const addNewPerson = (event) => {
         event.preventDefault();
-
+        
         // Prevent adding duplicates
         if (persons.find(person => person['name'] === newName)) {
             window.alert(`${newName} is already added to phonebook`);
             return;
         }
-
-        const newNameObject = {
-            name: newName
+        const newPersonObject = {
+            name: newName,
+            number: newNumber
         }
-        setPersons(persons.concat(newNameObject));
+        setPersons(persons.concat(newPersonObject));
         setNewName('');
+        setNewNumber('');
     }
 
     const handleNameChange = (event) => {
         setNewName(event.target.value);
     }
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value);
+    }
 
     return (
         <div>
-            <div>debug: {newName}</div>
             <h2>Phonebook</h2>
-            <form onSubmit={addNewName}>
+            <form onSubmit={addNewPerson}>
                 <div>
                     name: 
                     <input 
                       value={newName}
                       onChange={handleNameChange}
+                    />
+                </div>
+                <div>
+                    number:
+                    <input 
+                      value={newNumber}
+                      onChange={handleNumberChange}
                     />
                 </div>
                 <div>
@@ -45,7 +59,7 @@ const App = () => {
             <h2>Numbers</h2>
             <div>
                 {persons.map(person => 
-                    <p key={person.name}>{person.name}</p>
+                    <p key={person.name}>{person.name} {person.number}</p>
                 )}
             </div>
         </div>
