92236085472619efd83d2148e23258625aceb409
moryama171 <55793984+moryama171@users.noreply.github.com>
2020-11-28 11:23:15 +0100

    Resize image

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './index.css'
 import Filter from './components/Filter';
 import Display from './components/Display';
 
