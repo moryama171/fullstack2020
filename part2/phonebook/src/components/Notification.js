375b5362df012258054e1b9074978b0445308b53
moryama171 <55793984+moryama171@users.noreply.github.com>
2020-11-30 11:50:15 +0100

    Add style to Notification

--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 const Notification = ({ message }) => {
+    const notificationStyle = {
+        marginBottom: 25,
+        maxWidth: '60%',
+        background: '#AEE0A1',
+        border: 'solid #D1EEC9',
+        borderRadius: 5,
+        borderWidth: 2,
+        paddingLeft: '0.5em',
+        fontSize: 17
+    }
+    
     if (message === null) {
         return null;
     }
 
     return (
-        // TODO: Add style
-        <div>
-            {message}
+        <div style={notificationStyle}>
+           <p>{message}</p>
         </div>
     )
 }
