1bacb9d84a5ea93f9d42c4263646d53670b794a7
moryama171 <55793984+moryama171@users.noreply.github.com>
2020-12-21 11:53:08 +0100

    Implement create blog service

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,14 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
-export default { getAll }
+const create = async (newBlogObject) => {
+  const response = await axios.post(baseUrl, newBlogObject);
+  return response.data;
+};
+
+export default { create, getAll };
