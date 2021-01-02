d370e3a73681748f4f31aa0770600720da06c037
moryama171 <55793984+moryama171@users.noreply.github.com>
2021-01-02 11:22:33 +0100

    Fix: now list of blogs rerenders after any changes to the list itself

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -18,7 +18,7 @@ const App = () => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     );
-  }, []);
+  }, [blogs]);
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedUser');
